f5e24bc7108e96f748ba6978a31dd0afaa4b9cfa
tkaaad97 <tkaaad97@gmail.com>
2019-01-26 21:41:04 +0900

    global properties

--- run-master.sh
+++ run-master.sh
@@ -3,18 +3,22 @@
 set -uex -o pipefail
 
 usage_exit() {
-    echo "Usage: $0 [-J JMETER_PROPERTY] [-f SCENARIO_FILE_NAME] [-R REMOTE_HOSTS]" >&2
+    echo "Usage: $0 [-G JMETER_PROPERTY] [-J JMETER_PROPERTY] [-f SCENARIO_FILE_NAME] [-R REMOTE_HOSTS]" >&2
     exit 1
 }
 
+global_properties=()
 jmeter_properties=()
 AWS_REGION=${AWS_REGION:-ap-northeast-1}
 SCENARIO_FILE_NAME=${SCENARIO_STARTPOINT_FILE_NAME:-example.jmx}
 REMOTE_HOSTS=''
 
-while getopts J:f:R:h OPT
+while getopts G:J:f:R:h OPT
 do
     case $OPT in
+        G)
+            global_properties+=( "$OPTARG" )
+            ;;
         J)
             jmeter_properties+=( "$OPTARG" )
             ;;
@@ -41,6 +45,10 @@ jmeter_options=(-n -t "senarios/${SCENARIO_FILE_NAME}" -l "$logfile_name" '-Jser
 
 for prop in "${jmeter_properties[@]}"; do
     jmeter_options+=( -J"$prop" )
+done
+
+for prop in "${global_properties[@]}"; do
+    jmeter_options+=( -G"$prop" )
 done
 
 if [ -n "$REMOTE_HOSTS" ]; then
