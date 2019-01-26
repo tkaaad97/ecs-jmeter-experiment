db759d97a1033fce8a1958b1e37f1f103a2b1038
tkaaad97 <tkaaad97@gmail.com>
2019-01-26 18:28:38 +0900

    remote_hosts option

--- run-master.sh
+++ run-master.sh
@@ -3,16 +3,16 @@
 set -uex -o pipefail
 
 usage_exit() {
-    echo "Usage: $0 [OPTION...]
-    [-J JMETER_PROPERTY] [-f SCENARIO_FILE_NAME]" >&2
+    echo "Usage: $0 [-J JMETER_PROPERTY] [-f SCENARIO_FILE_NAME] [-R REMOTE_HOSTS]" >&2
     exit 1
 }
 
 jmeter_properties=()
 AWS_REGION=${AWS_REGION:-ap-northeast-1}
 SCENARIO_FILE_NAME=${SCENARIO_STARTPOINT_FILE_NAME:-example.jmx}
+REMOTE_HOSTS=''
 
-while getopts J:s:f:h OPT
+while getopts J:f:R:h OPT
 do
     case $OPT in
         J)
@@ -20,6 +20,9 @@ do
             ;;
         f)
             SCENARIO_FILE_NAME="$OPTARG"
+            ;;
+        R)
+            REMOTE_HOSTS="$OPTARG"
             ;;
         h)
             usage_exit
@@ -36,6 +39,10 @@ done
 logfile_name=result.jtl
 jmeter_options=(-n -t "senarios/${SCENARIO_FILE_NAME}" -l "$logfile_name")
 jmeter_options+=( $(printf -- '-J \"%s\" ' "${jmeter_properties[@]}") )
+
+if [ -n "$REMOTE_HOSTS" ]; then
+    jmeter_options+=( -R "$REMOTE_HOSTS" )
+fi
 
 jmeter "${jmeter_options[@]}"
 cat 'result.jtl'
